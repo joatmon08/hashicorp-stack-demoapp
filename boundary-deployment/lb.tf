be5acffc3f1b8a9976c72f0a3731bc476a237e8a
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-04-05 13:53:00 -0400

    Add egress rule to lb

--- boundary-deployment/lb.tf
+++ boundary-deployment/lb.tf
@@ -60,3 +60,12 @@ resource "aws_security_group_rule" "allow_9200" {
   cidr_blocks       = [var.client_cidr_block]
   security_group_id = aws_security_group.controller_lb.id
 }
+
+resource "aws_security_group_rule" "lb_egress" {
+  type                     = "egress"
+  from_port                = 9200
+  to_port                  = 9200
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.controller.id
+  security_group_id        = aws_security_group.controller_lb.id
+}
