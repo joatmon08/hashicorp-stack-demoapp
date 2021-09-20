005a011a9f126f17a390e20f1ad5e889cf661261
Kerim Satirli <k.satirli@gmail.com>
2021-09-20 14:19:01 +0200

    linting for readability

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,18 +1,22 @@
 terraform {
   required_version = "~> 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.52"
     }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.4"
     }
+
     tls = {
       source  = "hashicorp/tls"
       version = "~> 3.1"
     }
+
     hcp = {
       source  = "hashicorp/hcp"
       version = "~> 0.11"
@@ -22,6 +26,7 @@ terraform {
 
 provider "aws" {
   region = var.region
+
   default_tags {
     tags = var.tags
   }
--- infrastructure/terraform.auto.tfvars
+++ infrastructure/terraform.auto.tfvars
@@ -3,6 +3,7 @@ hcp_region                 = "us-east-1"
 name                       = "zero"
 hcp_consul_public_endpoint = true
 hcp_vault_public_endpoint  = true
+
 tags = {
   Environment = "rosemary-zero-trust-demo"
   Automation  = "terraform"
