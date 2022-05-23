88bb674a7818bb07e279625fff803f73a10dc26a
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2022-05-23 10:41:51 -0400

    Add Vault namespace to agent injector

--- consul/setup/kubernetes.tf
+++ consul/setup/kubernetes.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "consul" {
-  name       = "consul"
+  name = "consul"
 
   repository = "https://helm.releases.hashicorp.com"
   chart      = "consul"
@@ -13,6 +13,7 @@ resource "helm_release" "consul" {
       CONSUL_CA_ROLE              = local.roles.consul_ca
       SERVER_ACL_INIT_ROLE        = local.roles.server_acl_init
       VAULT_ADDR                  = local.vault_addr
+      VAULT_NAMESPACE             = local.vault_namespace
       KUBERNETES_AUTH_METHOD_PATH = local.paths.kubernetes_auth_method
       CONSUL_PKI_PATH             = local.paths.consul_pki
       CONSUL_STATIC_PATH          = local.paths.consul_static
--- consul/setup/variables.tf
+++ consul/setup/variables.tf
@@ -30,6 +30,7 @@ locals {
   paths             = data.terraform_remote_state.vault_consul.outputs.paths
   consul_datacenter = data.terraform_remote_state.vault_consul.outputs.consul_datacenter
   vault_addr        = data.terraform_remote_state.vault_consul.outputs.vault_addr
+  vault_namespace   = data.terraform_remote_state.vault_consul.outputs.vault_namespace
 }
 
 variable "consul_helm_version" {
