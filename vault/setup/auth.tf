fa43926a396dffba8b36ca3594fefcd0f2a10f46
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2022-05-23 10:25:36 -0400

    Disable ISS validation for Vault k8s auth method

--- vault/setup/auth.tf
+++ vault/setup/auth.tf
@@ -20,9 +20,10 @@ resource "vault_auth_backend" "kubernetes" {
 }
 
 resource "vault_kubernetes_auth_backend_config" "kubernetes" {
-  depends_on         = [helm_release.vault]
-  backend            = vault_auth_backend.kubernetes.path
-  kubernetes_host    = local.kubernetes_host
-  kubernetes_ca_cert = data.kubernetes_secret.vault_auth.data["ca.crt"]
-  token_reviewer_jwt = data.kubernetes_secret.vault_auth.data.token
+  depends_on             = [helm_release.vault]
+  backend                = vault_auth_backend.kubernetes.path
+  kubernetes_host        = local.kubernetes_host
+  kubernetes_ca_cert     = data.kubernetes_secret.vault_auth.data["ca.crt"]
+  token_reviewer_jwt     = data.kubernetes_secret.vault_auth.data.token
+  disable_iss_validation = "true"
 }
