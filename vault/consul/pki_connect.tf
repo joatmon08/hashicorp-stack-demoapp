986f5774a4179c9493329e1d9bb1f6f9d4c7a3e0
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2022-05-26 12:24:16 -0400

    Remove role

--- vault/consul/pki_connect.tf
+++ vault/consul/pki_connect.tf
@@ -8,16 +8,3 @@ resource "vault_pki_secret_backend_config_urls" "consul_connect" {
     "${data.hcp_vault_cluster.cluster.vault_private_endpoint_url}/v1/${var.vault_consul_connect_pki_root_backend}/crl"
   ]
 }
-
-resource "vault_pki_secret_backend_role" "consul_connect" {
-  backend = var.vault_consul_connect_pki_root_backend
-  name    = "consul-server"
-  max_ttl = 2592000
-  allowed_domains = [
-    "example.com"
-  ]
-  allow_subdomains   = true
-  allow_bare_domains = true
-  allow_localhost    = true
-  generate_lease     = true
-}
