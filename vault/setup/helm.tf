b9d41ddee5ea53e6216030674b8ef84973644c5d
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2022-05-26 16:42:12 -0400

    Add Secrets Store CSI Driver

--- vault/setup/helm.tf
+++ vault/setup/helm.tf
@@ -1,4 +1,23 @@
+resource "helm_release" "csi" {
+  name       = "csi"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+  chart      = "secrets-store-csi-driver"
+  version    = var.csi_helm_version
+
+  set {
+    name  = "enableSecretRotation"
+    value = "true"
+  }
+
+  set {
+    name  = "syncSecret.enabled"
+    value = "true"
+  }
+}
+
 resource "helm_release" "vault" {
+  depends_on = [helm_release.csi]
   name       = "vault"
   repository = "https://helm.releases.hashicorp.com"
   chart      = "vault"
@@ -12,5 +31,10 @@ resource "helm_release" "vault" {
   set {
     name  = "injector.externalVaultAddr"
     value = data.hcp_vault_cluster.cluster.vault_private_endpoint_url
+  }
+
+  set {
+    name  = "csi.enabled"
+    value = "true"
   }
 }
--- vault/setup/variables.tf
+++ vault/setup/variables.tf
@@ -61,3 +61,9 @@ variable "vault_helm_version" {
   description = "Vault Helm chart version"
   default     = "0.20.0"
 }
+
+variable "csi_helm_version" {
+  type        = string
+  description = "Secrets Store CSI Driver Helm chart version"
+  default     = "1.1.2"
+}
