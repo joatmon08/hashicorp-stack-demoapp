e65aec2252df07b3ad0123acf84e541af9c57ed0
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-05-18 14:04:03 -0400

    Add empty log/metric config to vault

--- infrastructure/hcp_vault.tf
+++ infrastructure/hcp_vault.tf
@@ -1,30 +1,17 @@
-locals {
-  datadog_config = (var.datadog_api_key != null && var.datadog_region != null) ? [{
-    api_key = var.datadog_api_key
-    region  = var.datadog_region
-  }] : []
-}
-
 resource "hcp_vault_cluster" "main" {
   cluster_id      = var.name
   hvn_id          = hcp_hvn.main.hvn_id
   public_endpoint = var.hcp_vault_public_endpoint
   tier            = var.hcp_vault_tier
 
-  dynamic "metrics_config" {
-    for_each = local.datadog_config
-    content {
-      datadog_api_key = metrics_config.value.api_key
-      datadog_region  = metrics_config.value.region
-    }
+  metrics_config {
+    datadog_api_key = var.datadog_api_key
+    datadog_region  = var.datadog_region
   }
 
-  dynamic "audit_log_config" {
-    for_each = local.datadog_config
-    content {
-      datadog_api_key = audit_log_config.value.api_key
-      datadog_region  = audit_log_config.value.region
-    }
+  audit_log_config {
+    datadog_api_key = var.datadog_api_key
+    datadog_region  = var.datadog_region
   }
 }
 
