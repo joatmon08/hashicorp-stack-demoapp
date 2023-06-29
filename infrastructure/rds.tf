76be48f1aac0a12719735446ae68548f32ad9194
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-06-29 12:45:31 -0400

    Add imported database

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -72,3 +72,52 @@ import {
   id = "products-v2"
   to = aws_db_instance.products_v2
 }
+
+resource "aws_db_instance" "products_v2" {
+  allocated_storage                     = 20
+  availability_zone                     = "${var.region}a"
+  db_subnet_group_name                  = module.vpc.database_subnet_group_name
+  delete_automated_backups              = true
+  deletion_protection                   = false
+  domain                                = null
+  domain_iam_role_name                  = null
+  enabled_cloudwatch_logs_exports       = []
+  engine                                = "postgres"
+  engine_version                        = "14.7"
+  final_snapshot_identifier             = null
+  iam_database_authentication_enabled   = false
+  identifier                            = "products-v2"
+  identifier_prefix                     = null
+  instance_class                        = "db.t3.micro"
+  iops                                  = 0
+  license_model                         = "postgresql-license"
+  maintenance_window                    = "thu:09:53-thu:10:23"
+  manage_master_user_password           = null
+  master_user_secret_kms_key_id         = null
+  max_allocated_storage                 = 0
+  monitoring_interval                   = 0
+  monitoring_role_arn                   = null
+  multi_az                              = false
+  nchar_character_set_name              = null
+  network_type                          = "IPV4"
+  option_group_name                     = "default:postgres-14"
+  parameter_group_name                  = "default.postgres14"
+  password                              = null # sensitive
+  performance_insights_enabled          = false
+  performance_insights_kms_key_id       = null
+  performance_insights_retention_period = 0
+  port                                  = 5432
+  publicly_accessible                   = false
+  replica_mode                          = null
+  replicate_source_db                   = null
+  skip_final_snapshot                   = true
+  snapshot_identifier                   = null
+  storage_encrypted                     = true
+  storage_throughput                    = 0
+  storage_type                          = "gp2"
+  tags                                  = {}
+  tags_all                              = {}
+  timezone                              = null
+  username                              = "postgres"
+  vpc_security_group_ids                = ["sg-0f0d10d09c631e6fa"]
+}
