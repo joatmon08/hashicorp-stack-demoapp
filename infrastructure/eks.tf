4b2be774643ac87b2e8b5d50edf142ab9a7e0f66
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-07-06 12:15:20 -0400

    Update Kubernetes cluster

--- infrastructure/eks.tf
+++ infrastructure/eks.tf
@@ -1,29 +1,29 @@
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "19.13.1"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.13.1"
 
-#   cluster_name    = var.name
-#   cluster_version = "1.26"
+  cluster_name    = var.name
+  cluster_version = "1.26"
 
-#   cluster_endpoint_public_access = true
+  cluster_endpoint_public_access = true
 
-#   vpc_id     = module.vpc.vpc_id
-#   subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-#   eks_managed_node_group_defaults = {
-#     create_iam_role = true
-#     ami_type        = "AL2_x86_64"
-#     disk_size       = 100
-#     instance_types  = ["m5.large"]
-#   }
+  eks_managed_node_group_defaults = {
+    create_iam_role = true
+    ami_type        = "AL2_x86_64"
+    disk_size       = 100
+    instance_types  = ["m5.large"]
+  }
 
-#   eks_managed_node_groups = {
-#     hashicups = {
-#       min_size     = 3
-#       max_size     = 5
-#       desired_size = 3
+  eks_managed_node_groups = {
+    hashicups = {
+      min_size     = 1
+      max_size     = 5
+      desired_size = 3
 
-#       instance_types = ["m5.large"]
-#     }
-#   }
-# }
+      instance_types = ["m5.large"]
+    }
+  }
+}
