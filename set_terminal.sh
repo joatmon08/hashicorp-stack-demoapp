9dd9a0e468b990a8e6130ca22f18ae95090ad1c1
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-07-06 15:03:48 -0400

    Add e2e tests

--- set_terminal.sh
+++ set_terminal.sh
@@ -1,8 +1,11 @@
-export BOUNDARY_ADDR=$(cd boundary && terraform output -raw boundary_endpoint)
+export BOUNDARY_ADDR=$(cd infrastructure && terraform output -raw hcp_boundary_endpoint)
 export PGPASSWORD=$(cd infrastructure && terraform output -raw product_database_password)
 export VAULT_ADDR=$(cd infrastructure && terraform output -raw hcp_vault_public_address)
 export VAULT_TOKEN=$(cd infrastructure && terraform output -raw hcp_vault_token)
 export VAULT_NAMESPACE=$(cd infrastructure && terraform output -raw hcp_vault_namespace)
-export CONSUL_HTTP_ADDR=$(cd consul/setup && terraform output -raw consul_address)
-export CONSUL_HTTP_TOKEN=$(cd consul/setup && terraform output -raw consul_token)
-export CONSUL_HTTP_SSL_VERIFY=false
+export CONSUL_HTTP_ADDR=$(cd infrastructure && terraform output -raw hcp_consul_public_address)
+export CONSUL_HTTP_TOKEN=$(cd infrastructure && terraform output -raw hcp_consul_token)
+
+# export CONSUL_HTTP_ADDR=$(cd consul/setup && terraform output -raw consul_address)
+# export CONSUL_HTTP_TOKEN=$(cd consul/setup && terraform output -raw consul_token)
+# export CONSUL_HTTP_SSL_VERIFY=false
