096a91a7af04b245bfb7416bb1058acfbd2a7322
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-09-07 10:08:27 -0400

    Add ArgoCD workspace

---
+++ bootstrap/argocd.tf
@@ -0,0 +1,24 @@
+resource "tfe_workspace" "argocd" {
+  name              = "argocd"
+  organization      = tfe_organization.demo.name
+  description       = "Step 3 - Set up ArgoCD on Kubernetes"
+  terraform_version = var.terraform_version
+  working_directory = "argocd"
+  trigger_prefixes  = ["argocd"]
+  queue_all_runs    = false
+  vcs_repo {
+    identifier     = var.github_repository
+    branch         = var.github_branch
+    oauth_token_id = tfe_oauth_client.github.oauth_token_id
+  }
+}
+
+resource "tfe_workspace_variable_set" "argocd_aws" {
+  workspace_id    = tfe_workspace.argocd.id
+  variable_set_id = tfe_variable_set.aws.id
+}
+
+resource "tfe_workspace_variable_set" "argocd_common" {
+  workspace_id    = tfe_workspace.argocd.id
+  variable_set_id = tfe_variable_set.common.id
+}
