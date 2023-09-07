4c960da0c585be1880ab9248835af82a56c1f9ab
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-09-07 16:13:03 -0400

    Change ArgoCD to loadbalancer service

--- argocd/helm.tf
+++ argocd/helm.tf
@@ -1,9 +1,10 @@
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  namespace  = "default"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = var.argocd_helm_version
+  name             = "argocd"
+  namespace        = "argocd"
+  create_namespace = true
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = var.argocd_helm_version
 
   values = [
     file("templates/values.yaml")
