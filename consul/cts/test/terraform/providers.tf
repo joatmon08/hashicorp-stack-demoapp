1bb133f7dbd09f31f96e3af0fd78cc1b7e477486
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-09-11 15:20:48 -0400

    Remove CTS automation

--- consul/cts/test/terraform/providers.tf
+++
@@ -1,8 +0,0 @@
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
-}
-
-provider "vault" {}
-
-provider "consul" {}
--- consul/cts/test/terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  postgres_username = "postgres"
-  postgres_password = "password"
-  kubernetes_host   = "https://192.168.59.100:8443"
-  name              = "product"
-  database_path     = "database"
-  vault_addr        = "http://vault:8200"
-}
