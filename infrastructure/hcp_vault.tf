ded54ec2f28dd50abf5116a3f4a1513dcd952e38
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-09-18 15:45:06 -0400

    Remove datadog from HCP vault

--- infrastructure/hcp_vault.tf
+++ infrastructure/hcp_vault.tf
@@ -8,15 +8,15 @@ resource "hcp_vault_cluster" "main" {
   public_endpoint = var.hcp_vault_public_endpoint
   tier            = var.hcp_vault_tier
 
-  metrics_config {
-    datadog_api_key = var.datadog_api_key
-    datadog_region  = var.datadog_region
-  }
+  # metrics_config {
+  #   datadog_api_key = var.datadog_api_key
+  #   datadog_region  = var.datadog_region
+  # }
 
-  audit_log_config {
-    datadog_api_key = var.datadog_api_key
-    datadog_region  = var.datadog_region
-  }
+  # audit_log_config {
+  #   datadog_api_key = var.datadog_api_key
+  #   datadog_region  = var.datadog_region
+  # }
 
   lifecycle {
     postcondition {
