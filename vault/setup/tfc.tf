a99163746a54dbc1ef72478febb3acbeec5337ea
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-09-18 14:19:33 -0400

    Add policy per application team

--- vault/setup/tfc.tf
+++ vault/setup/tfc.tf
@@ -11,3 +11,26 @@ resource "vault_terraform_cloud_secret_role" "apps" {
   organization = var.tfc_organization
   team_id      = each.value
 }
+
+resource "vault_policy" "tfc_secrets_engine" {
+  for_each = toset(keys(var.tfc_team_ids))
+  name     = "tfc-secrets-engine-${each.value}"
+
+  policy = <<EOT
+path "terraform/creds/${each.value}" {
+  capabilities = [ "read" ]
+}
+EOT
+}
+
+resource "vault_kubernetes_auth_backend_role" "tfc_token" {
+  for_each                         = toset(keys(var.tfc_team_ids))
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "terraform-cloud-operator"
+  bound_service_account_names      = ["terraform-cloud"]
+  bound_service_account_namespaces = [each.value]
+  token_ttl                        = 86400
+  token_policies = [
+    vault_policy.tfc_secrets_engine[each.value].name
+  ]
+}
