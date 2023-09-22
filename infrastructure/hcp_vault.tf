4cb2f2f97f5172919af8fa48020f72e70a82f0bd
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-09-22 13:53:41 -0400

    Remove postcondition from HCP Vault

--- infrastructure/hcp_vault.tf
+++ infrastructure/hcp_vault.tf
@@ -17,13 +17,6 @@ resource "hcp_vault_cluster" "main" {
   #   datadog_api_key = var.datadog_api_key
   #   datadog_region  = var.datadog_region
   # }
-
-  lifecycle {
-    postcondition {
-      condition     = replace(self.vault_version, "v", "") != jsondecode(data.http.vault_version.response_body).version
-      error_message = "Avoid using latest Vault version until approved"
-    }
-  }
 }
 
 resource "hcp_vault_cluster_admin_token" "cluster" {
