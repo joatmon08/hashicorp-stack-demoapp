47f9e1b9e5fb19b3dfce9388c171b9cc109912d2
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-09-29 13:31:00 -0400

    Remove database policy from consul setup

--- vault/consul/consul_tgw.tf
+++
@@ -1,8 +0,0 @@
-resource "consul_acl_policy" "terminating_gateway_database" {
-  name  = "database-write-policy"
-  rules = <<-RULE
-service_prefix "database" {
-    policy = "write"
-}
-    RULE
-}
--- vault/consul/outputs.tf
+++ vault/consul/outputs.tf
@@ -26,7 +26,3 @@ output "consul_api_gateway_allowed_domain" {
 output "consul_datacenter" {
   value = var.consul_datacenter
 }
-
-output "consul_tgw_database_policy" {
-  value = consul_acl_policy.terminating_gateway_database.name
-}
