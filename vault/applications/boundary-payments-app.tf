f2e6a67f6774439d7b6e8c50d11470e8faeee198
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-10-05 15:30:17 -0400

    Add payments-processor endpoint

--- vault/applications/boundary-payments-app.tf
+++ vault/applications/boundary-payments-app.tf
@@ -1,7 +1,3 @@
-data "aws_eks_cluster" "cluster" {
-  name = local.eks_cluster_name
-}
-
 resource "boundary_target" "kubernetes" {
   type                     = "tcp"
   name                     = "kubernetes"
@@ -14,5 +10,85 @@ resource "boundary_target" "kubernetes" {
   default_port             = 443
   brokered_credential_source_ids = [
     boundary_credential_library_vault.application["payments-app"].id
+  ]
+}
+
+data "kubernetes_service" "payments_processor" {
+  metadata {
+    name      = "payments-processor"
+    namespace = "payments-app"
+  }
+}
+
+resource "vault_policy" "token_basics" {
+  name   = "token-basics"
+  policy = <<EOT
+path "auth/token/lookup-self" {
+  capabilities = ["read"]
+}
+
+path "auth/token/renew-self" {
+  capabilities = ["update"]
+}
+
+path "auth/token/revoke-self" {
+  capabilities = ["update"]
+}
+
+path "sys/leases/renew" {
+  capabilities = ["update"]
+}
+
+path "sys/leases/revoke" {
+  capabilities = ["update"]
+}
+
+path "sys/capabilities-self" {
+  capabilities = ["update"]
+}
+EOT
+}
+
+resource "vault_token" "payments_processor" {
+  policies          = [vault_policy.token_basics.name, vault_policy.payments_processor.name]
+  no_default_policy = true
+  no_parent         = true
+  ttl               = "3d"
+  explicit_max_ttl  = "6d"
+  period            = "3d"
+  renewable         = true
+  num_uses          = 0
+}
+
+resource "boundary_credential_store_vault" "payments_processor" {
+  name        = "payments-processor"
+  description = "Vault credentials store for payments-processor"
+  address     = local.vault_address
+  token       = vault_token.payments_processor.client_token
+  namespace   = local.vault_namespace
+  scope_id    = boundary_scope.apps["payments-app"].id
+}
+
+resource "boundary_credential_library_vault" "payments_processor" {
+  name                = "payments-processor"
+  description         = "Credential library for payments-processor"
+  credential_store_id = boundary_credential_store_vault.payments_processor.id
+  path                = "payments-processor/static/data/creds"
+  http_method         = "GET"
+  credential_type     = "username_password"
+}
+
+resource "boundary_target" "payments_processor" {
+  type                     = "tcp"
+  name                     = "payments-processor"
+  description              = "Payments processor API"
+  scope_id                 = boundary_scope.apps["payments-app"].id
+  ingress_worker_filter    = "\"eks\" in \"/tags/type\""
+  egress_worker_filter     = "\"${local.name}\" in \"/tags/type\""
+  address                  = data.kubernetes_service.payments_processor.status.0.load_balancer.0.ingress.0.hostname
+  session_connection_limit = -1
+  default_port             = 8080
+  brokered_credential_source_ids = [
+    boundary_credential_library_vault.payments_processor.id
   ]
 }
--- vault/applications/providers.tf
+++ vault/applications/providers.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = "~> 1.0"
   required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.13"
+    }
     vault = {
       source  = "hashicorp/vault"
       version = "~> 3.6"
@@ -22,6 +26,20 @@ terraform {
 
 provider "aws" {
   region = local.region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = local.eks_cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.eks_cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "vault" {
