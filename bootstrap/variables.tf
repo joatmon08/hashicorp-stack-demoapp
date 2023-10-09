fa80b35af12d9d9fb4d1c0b68fd39f8033b9da7a
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-10-09 14:41:45 -0400

    Downgrade to 1.5.7

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,7 +1,7 @@
 variable "terraform_version" {
   type        = string
   description = "Terraform version for all workspaces"
-  default     = "1.6.0"
+  default     = "1.5.7"
 }
 
 variable "tfc_organization" {
