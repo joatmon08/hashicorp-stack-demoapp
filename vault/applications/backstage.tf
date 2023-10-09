483b8c1ca1000df004ea2d2555eb99a2fc9c1309
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-10-09 13:36:14 -0400

    Allow user to get hashicups

--- vault/applications/backstage.tf
+++ vault/applications/backstage.tf
@@ -1,5 +1,5 @@
 resource "vault_github_user" "backstage" {
   backend  = local.vault_github_auth_path
   user     = var.github_user
-  policies = [vault_policy.tfc_secrets_engine["backstage"].name]
+  policies = [vault_policy.tfc_secrets_engine["hashicups"].name]
 }
