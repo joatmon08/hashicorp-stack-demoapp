6874c18ca0114346daa200c3645cbae92254b24c
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-06-24 13:28:32 -0400

    Create waypoint add-on for postgresql

--- infrastructure/hcp_waypoint.tf
+++ infrastructure/hcp_waypoint.tf
@@ -17,3 +17,19 @@ resource "hcp_waypoint_application_template" "spring_postgres" {
     options       = [module.vpc.database_subnet_group_name]
   }]
 }
+
+resource "hcp_waypoint_add_on_definition" "aws_rds_postgresql" {
+  name        = "aws-rds-postgresql"
+  summary     = "AWS RDS managed instance with PostgreSQL"
+  description = "Creates an AWS RDS instance with PostgreSQL"
+
+  terraform_cloud_workspace_details = {
+    name                 = "database"
+    terraform_project_id = var.waypoint_terraform_project_id
+  }
+
+  terraform_no_code_module = {
+    source  = "app.terraform.io/hashicorp-stack-demoapp/postgres-waypoint/aws"
+    version = "0.0.0"
+  }
+}
